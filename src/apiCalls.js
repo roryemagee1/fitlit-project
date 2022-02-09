502d972d067226dd5ca01114f9890d8d364e8164
Sam Mistele <smmstl0@gmail.com>
2022-02-09 15:40:18 -0700

    Add user data fetch request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,9 @@
 // Your fetch requests will live here!
-
+const userData = () => {
+  return fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+  .then((response => response.json())
+  .then((data) => data)
+  .catch((err) => console.log('ERROR')))
+}
 
 console.log('I will be a fetch request!')
-
