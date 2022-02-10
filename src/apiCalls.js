ca7856bb33a5778762908c2c7dcefe87848de142
Rory Magee <roryemagee@gmail.com>
2022-02-10 15:36:34 -0700

    Add basic page display

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,48 +1,11 @@
 // Your fetch requests will live here!
 const getUserData = fetch('https://fitlit-api.herokuapp.com/api/v1/users').then(response => response.json());
-//   .then(response => response.json())
-//   .then(data => data.userData)
-//   .catch(err => console.log('ERROR'))
-// }
 
 const getHydrationData = fetch('https://fitlit-api.herokuapp.com/api/v1/hydration').then(response => response.json());
-//   .then(response => response.json())
-//   .then(data => data.hydrationData)
-//   .catch(err => console.log('ERROR'))
-// }
 
 const getSleepData = fetch('https://fitlit-api.herokuapp.com/api/v1/sleep').then(response => response.json());
-//   .then(response => response.json())
-//   .then(data => data.sleepData)
-//   .catch(err => console.log('ERROR'))
-// }
 
-// Promise.all([getUserData, getHydrationData, getSleepData])
-// .then(files => {
-//   files.forEach(file => {
-//     process(file.json());
-//   })
-// })
-//   .catch(err =>{
-//
-//   });
-//
-// let process = prom => {
-//   prom.then(data => {
-//
-//   })
-// }
 
-// const userData = () => {
-//   return Promise.all([
-// 	getUserData,
-// 	getHydrationData,
-// 	getSleepData,
-// ]).then(data => {
-//   newRepo = new UserRepository(data[0])
-//   newUser = new User(newRepo.allData[0][1])
-// })
-// }
 
 export {getUserData, getSleepData, getHydrationData};
 
--- src/scripts.js
+++ src/scripts.js
@@ -2,97 +2,47 @@
 // Do not delete or rename this file ********
 import './css/styles.css';
 import './images/turing-logo.png'
-// import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
-// import userData from './apiCalls.js';
 import {getUserData, getSleepData, getHydrationData} from './apiCalls.js'
+
 // QUERY SELECTORS
 const welcomeBox = document.querySelector('.welcome-box');
 const userStepGoalBox = document.querySelector('.user-step-goals');
 
-//EVENT LISTENERS
-let newUser;
-let newRepo;
-let theData;
+// EVENT LISTENERS
 
+// DOM
 Promise.all([getUserData, getHydrationData, getSleepData]).then(data => {
-  console.log(data);
-  newRepo = new UserRepository(data[0].userData);
-  newUser = new User(newRepo.allData[1]);
-  console.log(newRepo);
-  console.log(newUser);
-  testDataOutput();
-  });
+  let newRepo = new UserRepository(data[0].userData);
+  let jarvis = new User(newRepo.allData[1]);
+  updateMainBox(jarvis, newRepo);
+});
 
-function testDataOutput() {
-  welcomeBox.innerHTML += `<h1> ${newUser.name} is free! </h1>`;
-}
-
-
-
-// const x = () => {
-//   return Promise.all([
-//     getUserData,
-//     getHydrationData,
-//     getSleepData,
-//   ]).then(data => {
-//     newRepo = new UserRepository(data[0])
-//     console.log(newRepo)
-//     newUser = new User(newRepo.allData[0][1])
-//     console.log(newUser)
-//   }).catch(err => console.log(err))
-// }
-
-// FUNCTIONS
-// fetchData()
-
-function newUserInstance() {
-
-};
-
-// function newRepositoryInstance() {
-//   const newRepo = new UserRepository(fetchData());
-//   return newRepo;
-// }
-
-function welcomeUser() {
-  const newUser = newUserInstance();
-  console.log(newUserInstance())
-  const welcome = newUser.returnFirstName();
-  return welcome;
-};
-
-function updateMainBox() {
-  console.log('hi')
+function updateMainBox(person, repo) {
   welcomeBox.innerHTML += `
-    <h1>Welcome back, ${welcomeUser()}</h1>
+    <h1>Welcome back, ${person.returnFirstName()}</h1>
     <p>Email:</p>
-    <p>${newUserInstance().email}</p>
+    <p>${person.email}</p>
     <p>Friends: </p>
-    <p>${newUserInstance().friends.length}</p>
+    <p>${person.friends.length}</p>
     <div class="average-step">
-      <p>Your weekly step goal: </p>
-      <p>${newUserInstance().dailyStepGoal} </p>
+      <p>Your step goal: </p>
+      <p>${person.dailyStepGoal} </p>
       <p>Average user step goal: </p>
-      <p>${newRepositoryInstance().averageStepGoal()} </p>
+      <p>${repo.averageStepGoal()} </p>
     </div>`
 };
 
 
 
-window.addEventListener('load', x())
-welcomeBox.addEventListener('load', updateMainBox());
-userStepGoalBox.addEventListener('load', updateStepGoalBox());
 
 
 
 
 
 
-
-
-console.log(userData,"<>>>>userData")
+// console.log(userData,"<>>>>userData")
 // An example of how you tell webpack to use a CSS file
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
