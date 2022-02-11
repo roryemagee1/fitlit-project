9e1b282d6785d42433a751409cccd633015e3b49
Sam Mistele <smmstl0@gmail.com>
2022-02-11 15:48:43 -0700

    Add sleep class skeleton

---
+++ src/Sleep.js
@@ -0,0 +1,13 @@
+class Sleep {
+  constructor(id, sleepRepo) {
+    this.id = id;
+    this.sleepRepo = sleepRepo.allData.sleepData;
+    console.log(this.sleepRepo)
+  }
+  avgHoursSlept() {
+    
+  }
+
+}
+
+export default Sleep;
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,12 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 import './css/styles.css';
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 import UserRepository from './UserRepository';
 import User from './User';
-import Hydration from './Hydration.js'
-import {getUserData, getSleepData, getHydrationData} from './apiCalls.js'
+import Hydration from './Hydration.js';
+import Sleep from './Sleep.js';
+import {getUserData, getSleepData, getHydrationData} from './apiCalls.js';
 
 
 // QUERY SELECTORS
@@ -22,9 +23,10 @@ Promise.all([getUserData, getHydrationData, getSleepData]).then(data => {
   let newRepo = new UserRepository(data[0].userData);
   let hydrationRepo = new UserRepository(data[1]);
   let sleepRepo = new UserRepository(data[2]);
+  console.log(sleepRepo)
   let jarvis = new User(newRepo.allData[1]);
   let hydration = new Hydration(jarvis.id, hydrationRepo);
-  let sleep = new Sleep();
+  let sleep = new Sleep(jarvis.id, sleepRepo);
   updateMainBox(jarvis, newRepo);
   updateHydrationBox(hydration);
 });
