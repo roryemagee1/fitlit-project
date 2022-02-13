451a95a2f7fecb7189f5638302b10dfde53113f6
Rory Magee <roryemagee@gmail.com>
2022-02-13 14:57:01 -0700

    Fix hydration data file

--- src/data/hydration-sample.js
+++ src/data/hydration-sample.js
@@ -1,21 +1,33 @@
 let hydrationSample = [
-{userID: 1, date: '2019/06/15', numOunces: 37},
-{userID: 2, date: '2019/06/15', numOunces: 75},
-{userID: 3, date: '2019/06/15', numOunces: 47},
-{userID: 4, date: '2019/06/15', numOunces: 85}
+  {userID: 1, date: '2019/06/15', numOunces: 37},
+  {userID: 2, date: '2019/06/15', numOunces: 75},
+  {userID: 1, date: '2019/06/16', numOunces: 69},
+  {userID: 2, date: '2019/06/16', numOunces: 91},
+  {userID: 1, date: '2019/06/17', numOunces: 96},
+  {userID: 2, date: '2019/06/17', numOunces: 96},
+  {userID: 1, date: '2019/06/18', numOunces: 61},
+  {userID: 2, date: '2019/06/18', numOunces: 70},
+  {userID: 1, date: '2019/06/19', numOunces: 91},
+  {userID: 2, date: '2019/06/19', numOunces: 76},
+  {userID: 1, date: '2019/06/20', numOunces: 50},
+  {userID: 2, date: '2019/06/20', numOunces: 71},
+  {userID: 1, date: '2019/06/21', numOunces: 50},
+  {userID: 2, date: '2019/06/21', numOunces: 27},
+  {userID: 1, date: '2019/06/22', numOunces: 43},
+  {userID: 2, date: '2019/06/22', numOunces: 58},
+  {userID: 1, date: '2019/06/23', numOunces: 39},
+  {userID: 2, date: '2019/06/23', numOunces: 44},
+  {userID: 1, date: '2019/06/24', numOunces: 61},
+  {userID: 2, date: '2019/06/24', numOunces: 33},
+  {userID: 1, date: '2019/06/25', numOunces: 51},
+  {userID: 2, date: '2019/06/25', numOunces: 67},
+  {userID: 1, date: '2019/06/26', numOunces: 52},
+  {userID: 2, date: '2019/06/26', numOunces: 27},
+  {userID: 1, date: '2019/06/27', numOunces: 29},
+  {userID: 2, date: '2019/06/27', numOunces: 70},
+  {userID: 1, date: '2019/06/28', numOunces: 57},
+  {userID: 2, date: '2019/06/28', numOunces: 56},
+  {userID: 1, date: '2019/06/29', numOunces: 99}
 ]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default Hydration Sample;
+export default hydrationSample;
