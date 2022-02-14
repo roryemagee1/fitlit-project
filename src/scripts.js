a1325ee4f2f4542548dc04780758f191de3581ee
Sam Mistele <smmstl0@gmail.com>
2022-02-14 16:31:54 -0700

    refactor scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 import './css/styles.css';
 import './images/turing-logo.png';
 import UserRepository from './UserRepository';
@@ -26,43 +24,53 @@ Promise.all([getUserData, getHydrationData, getSleepData]).then(data => {
   updateMainBox(jarvis, newRepo);
   updateHydrationBox(hydration);
   updateSleepBox(sleep);
+  console.log(showFriendsNames(jarvis, newRepo))
 });
 
+function showFriendsNames(person, dataRepo) {
+  const getFriends = dataRepo.allData.filter(data => person.friends.includes(data.id)).map(data => data.name)
+  return getFriends;
+}
 function updateMainBox(person, repo) {
   welcomeBox.innerHTML += `
-    <h1>Welcome back, ${person.returnFirstName()}</h1>
-    <p>Email:</p>
+    <h1 class="welcome-tag">Welcome Back,</h1>
+    <h2 class="name">${person.returnFirstName().toUpperCase()}</h2>
+    <p><b>Email:</b></p>
     <p>${person.email}</p>
-    <p>Friends: </p>
-    <p>${person.friends.length}</p>
-    <div class="average-step">
-      <p>Your step goal: </p>
-      <p>${person.dailyStepGoal} </p>
-      <p>Average user step goal: </p>
-      <p>${repo.averageStepGoal()} </p>
+    <p><b>Friends:</b></p>
+    <p>${showFriendsNames(person, repo)}</p>
+    <div class="step">
+      <div class="step-goal">
+        <p><b>Your step goal:</b></p>
+        <p>${person.dailyStepGoal} </p>
+      </div>
+      <div class="average-step">
+        <p><b>Average user step goal:</b></p>
+        <p>${repo.averageStepGoal()} </p>
+      </div>
     </div>`
 };
 
 function updateHydrationBox(hydraRepo) {
   hydrationBox.innerHTML += `
-  <p>Today's Water Consumption</p>
+  <p><b>Today's Water Consumption</b></p>
   <p>${hydraRepo.showTodaysOz()}</p>
-  <p>Water Consumption Over Last 7 Days</p>
+  <p><b>Water Consumption Over Last 7 Days</b></p>
   <p>${hydraRepo.showWeeklyOz()}</p>`
 }
 
 function updateSleepBox(sleepRepo) {
   sleepBox.innerHTML += `
-  <p>Hours Slept from last week</p>
+  <p><b>Hours Slept from last week</b></p>
   <p>${sleepRepo.hoursSleptWeek('2020/01/16')}</p>
-  <p>Sleep Quality from last week</p>
+  <p><b>Sleep Quality from last week</b></p>
   <p>${sleepRepo.qualitySleepWeek('2020/01/16')}</p>
-  <p>Quality of Sleep Yesterday</p>
+  <p><b>Quality of Sleep Yesterday</b></p>
   <p>${sleepRepo.sleepQualityDay('2020/01/22')}</p>
-  <p>Hours Slept Yesterday</p>
+  <p><b>Hours Slept Yesterday</b></p>
   <p>${sleepRepo.hoursSleptDay('2020/01/22')}</p>
-  <p>All Time Average Sleep Quality</p>
+  <p><b>All Time Average Sleep Quality</b></p>
   <p>${sleepRepo.avgSleepQuality()}</p>
-  <p>All Time Average Hours Slept</p>
+  <p><b>All Time Average Hours Slept</b></p>
   <p>${sleepRepo.avgHoursSlept()}</p>`
 }
